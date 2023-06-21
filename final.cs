7f04fc0d2a0470b190b81c116d3ea3da61894d8f
Alecsander Zdanevich <alecsdjkzda@gmail.com>
2023-06-21 12:32:12 +0300

    //-!2! - формирует новый массив из строк

--- final.cs
+++ final.cs
@@ -3,7 +3,8 @@
 void TZ()
 {
     Console.WriteLine("Написать программу, \n" +
-                "!1! - которая из имеющегося массива строк \n" +
+                "!1! - которая из имеющегося массива строк
+                 \n" +
                 "!2! - формирует новый массив из строк, \n" +
                 "!3! - длина которых меньше, либо равна 3 символам.\n" +
                 "!4-!выбор! - Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.\n" +
@@ -11,4 +12,27 @@ void TZ()
 }
 TZ();
 
+//-!2! - формирует новый массив из строк
+string[] FillStringArray(string message)
+{
+    string[] result = new string[0]; int size = 0;
+        try
+        {
+            Console.WriteLine(message);
+            string some_str = "";string user_input ="";
+            while(user_input != "stop")
+            {
+                some_str = some_str + "<>" + user_input;
+                size++;
+                Console.Write($"!ЧТОБЫ ПРЕКРАТИТЬ ВВЕДИ stop!введите зачение № {size}: ");
+                user_input = Console.ReadLine()!;
+            }
+            Array.Resize(ref result,result.Length+size);
+            result = some_str.Split("<>");
+            Console.Write($"создан тестовый массив [{String.Join(" , ",result)}]");
+        }
+        catch{FillStringArray("\nошибка метода Prompt,попробуйте ввести данные снова: ");}
+    return result;
+}
+string[] string_array_first = FillStringArray("прошу ввести с клавиатуры данные для первоначального массива: ");
 
