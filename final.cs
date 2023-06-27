1267e77ccbc1fb30fd822b63f92acd501a530578
Alecsander Zdanevich <alecsdjkzda@gmail.com>
2023-06-27 07:47:36 +0300

    !3!-вывести на экран массив.длина которых меньше, либо равна 3 символам

--- final.cs
+++ final.cs
@@ -3,8 +3,7 @@
 void TZ()
 {
     Console.WriteLine("Написать программу, \n" +
-                "!1! - которая из имеющегося массива строк
-                 \n" +
+                "!1! - которая из имеющегося массива строк\n" +
                 "!2! - формирует новый массив из строк, \n" +
                 "!3! - длина которых меньше, либо равна 3 символам.\n" +
                 "!4-!выбор! - Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.\n" +
@@ -19,20 +18,35 @@ string[] FillStringArray(string message)
         try
         {
             Console.WriteLine(message);
-            string some_str = "";string user_input ="";
-            while(user_input != "stop")
-            {
-                some_str = some_str + "<>" + user_input;
-                size++;
-                Console.Write($"!ЧТОБЫ ПРЕКРАТИТЬ ВВЕДИ stop!введите зачение № {size}: ");
-                user_input = Console.ReadLine()!;
-            }
+                string some_str = "";string user_input ="";
+                while(user_input.ToLower() != "stop")
+                {
+                    size++;
+                    Console.Write($"!ЧТОБЫ ПРЕКРАТИТЬ ВВЕДИ stop!введите зачение № {size}: ");
+                    user_input = Console.ReadLine()!;
+                    if(user_input.ToLower() != "stop") some_str = some_str + user_input+"|";
+                }
             Array.Resize(ref result,result.Length+size);
-            result = some_str.Split("<>");
-            Console.Write($"создан тестовый массив [{String.Join(" , ",result)}]");
+            result = some_str.Split("|");
         }
         catch{FillStringArray("\nошибка метода Prompt,попробуйте ввести данные снова: ");}
     return result;
 }
-string[] string_array_first = FillStringArray("прошу ввести с клавиатуры данные для первоначального массива: ");
+string[] string_array = FillStringArray("прошу ввести с клавиатуры данные для первоначального массива: ");
 
+                                    //-вывести на экран массив
+                                    PritStringArray(string_array);
+                                    void PritStringArray(string[] array)
+                                    {
+                                        foreach(var el in array){Console.Write($"{el}\t");}
+                                    }
+//-!3! - длина которых меньше, либо равна 3 символам
+LeftThreeCharOfArray(string_array);
+void LeftThreeCharOfArray(string[] array)
+{
+    Console.Write("\n\nДанные, длина которых меньше, либо равна 3 символам: ");
+    foreach(var el in array)
+    {
+        if(el.Length<=3) Console.Write($"{el}\t");
+    }
+}
